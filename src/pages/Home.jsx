c84722c0f1b18b5dec1a2c1b004b187bc0bb2465
sds <sds@example.com>
2024-10-08 10:08:30 +0900

    add story 3, fix TextField comp css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import TextField from "../components/TextField.jsx";
+import styles from "./Home.module.css"
 
 export default function Home() {
     return (
-        <>
-            <TextField showBorder/>
-            <TextField/>
-        </>
+        <div className={styles['home']}>
+            <div className={styles['welcome-message-wrapper']}>
+                {'Welcome Newbie!!\nMyTodo makes it easy to stay organized and manage your life.'}
+            </div>
+            <div className={styles['title-wrapper']}>
+                {'What is your name?'}
+            </div>
+            <div className={styles['text-field-wrapper']}>
+                <TextField placeholder={'Input your name'}/>
+            </div>
+        </div>
     )
 }
